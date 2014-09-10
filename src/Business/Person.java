448703df0c198dc11ad8f8bac47ba6e8b5045bcd
chlia <chlia@chlia.redmond.corp.microsoft.com>
2014-09-09 23:15:30 -0700

    initial

---
+++ src/Business/Person.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Business;
+
+
+/**
+ *
+ * @author graceshi
+ */
+public class Person{
+    String firstname;
+    String lastname;
+    String dateofbirth;
+    String ssn;
+   
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getDateofbirth() {
+        return dateofbirth;
+    }
+
+    public void setDateofbirth(String dateofbirth) {
+        this.dateofbirth = dateofbirth;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+
+
+    
+    
+    
+}
